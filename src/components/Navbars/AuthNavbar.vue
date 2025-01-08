13ba640c1631412faa8d2acf0508f30b8888d5d7
sanwalsulehri <sanwalsulehri077@gmail.com>
2025-01-08 17:48:06 +0500

    first commit

---
+++ src/components/Navbars/AuthNavbar.vue
@@ -0,0 +1,102 @@
+<template>
+  <nav
+    class="top-0 absolute z-50 w-full flex flex-wrap items-center justify-between px-2 py-3 navbar-expand-lg"
+  >
+    <div
+      class="container px-4 mx-auto flex flex-wrap items-center justify-between"
+    >
+      <div
+        class="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start"
+      >
+        <router-link
+          class="text-white text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase"
+          to="/"
+        >
+          Sumar2
+        </router-link>
+        <button
+          class="cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none"
+          type="button"
+          v-on:click="setNavbarOpen"
+        >
+          <i class="text-white fas fa-bars"></i>
+        </button>
+      </div>
+      <div
+        class="lg:flex flex-grow items-center bg-white lg:bg-opacity-0 lg:shadow-none"
+        :class="[navbarOpen ? 'block rounded shadow-lg' : 'hidden']"
+        id="example-navbar-warning"
+      >
+      
+        <ul class="flex flex-col lg:flex-row list-none lg:ml-auto">
+         
+          <li class="flex items-center">
+            <a
+              class="lg:text-white lg:hover:text-blueGray-200 text-blueGray-700 px-3 py-4 lg:py-2 flex items-center text-xs uppercase font-bold"
+              href="https://www.instagram.com/sanwal_sulehriaa/"
+              target="_blank"
+            >
+              <i
+                class="lg:text-blueGray-200 text-blueGray-400 fab fa-instagram text-lg leading-lg"
+              />
+              <span class="lg:hidden inline-block ml-2">Share</span>
+            </a>
+          </li>
+
+          <li class="flex items-center">
+            <a
+              class="lg:text-white lg:hover:text-blueGray-200 text-blueGray-700 px-3 py-4 lg:py-2 flex items-center text-xs uppercase font-bold"
+              href="https://x.com/Mosanwal88"
+              target="_blank"
+            >
+              <i
+                class="lg:text-blueGray-200 text-blueGray-400 fab fa-twitter text-lg leading-lg"
+              />
+              <span class="lg:hidden inline-block ml-2">Tweet</span>
+            </a>
+          </li>
+
+          <li class="flex items-center">
+            <a
+              class="lg:text-white lg:hover:text-blueGray-200 text-blueGray-700 px-3 py-4 lg:py-2 flex items-center text-xs uppercase font-bold"
+              href="https://github.com/sanwalsulehri"
+              target="_blank"
+            >
+              <i
+                class="lg:text-blueGray-200 text-blueGray-400 fab fa-github text-lg leading-lg"
+              />
+              <span class="lg:hidden inline-block ml-2">Star</span>
+            </a>
+          </li>
+
+          <li class="flex items-center">
+            <router-link to="/auth/login">
+            <button
+              class="bg-white active:bg-white/20 text-xs font-bold uppercase px-4 py-2 rounded shadow hover:shadow-lg outline-none focus:outline-none lg:mr-1 lg:mb-0 ml-3 mb-3 ease-linear transition-all duration-150"
+              type="button"
+            >
+            <i class="fa-solid fa-door-open"></i> Sign In
+            </button>
+          </router-link>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      navbarOpen: false,
+    };
+  },
+  methods: {
+    setNavbarOpen: function () {
+      this.navbarOpen = !this.navbarOpen;
+    },
+  },
+  components: {
+  },
+};
+</script>
